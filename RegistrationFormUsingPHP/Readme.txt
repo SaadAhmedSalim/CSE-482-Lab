11497922a29c4616377aa5f1eeaf0360056410f7
SaadSalim <39336173+SaadAhmedSalim@users.noreply.github.com>
2020-07-31 23:00:06 +0600

    Week 2 Added

---
+++ RegistrationFormUsingPHP/Readme.txt
@@ -0,0 +1,10 @@
+ReadME File for My Server 
+Access Link :
+
+http://saadsalim278.byethost15.com/RegistrationFormUsingPHP/registrations.html
+
+I turned off the inspect elements view.
+
+http://saadsalim278.byethost15.com/RegistrationFormUsingPHP/registration.html
+
+Here source code can be seen.
---
+++ RegistrationFormUsingPHP/output.php
@@ -0,0 +1,27 @@
+<?php
+
+$name = $_GET["name"];
+$username = $_GET["username"];
+$email = $_GET["email"];
+$contact = $_GET["contact"];
+$gender = $_GET["gender"];
+$skills = $_GET["skills"];
+$psw = $_GET["psw"];
+$repsw = $_GET["psw-repeat"];
+
+
+if($psw.md5(1)!=$repsw.md5(1))
+{
+  echo "Password Not Match !";
+} else echo "Password Matched !";
+
+echo "<br>";
+echo "Name : " .$name."<br>";
+echo "Username : " .$username."<br>";
+echo "Email : " .$email. "<br>";
+echo "Password : " .$psw.md5(1). "<br>";
+echo "Contact No : " .$contact. "<br>";
+echo "Gender : " .$gender. "<br>";
+echo "Programming Skills : " .$skills. "<br>";
+
+?>
